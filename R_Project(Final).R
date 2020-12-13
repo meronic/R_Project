455e837c03b1221ca3faf1028e63b380067a27ac
명현 <meronic@naver.com>
2020-12-13 09:00:51 +0900

    test 3

--- R_Project(Final).R
+++ R_Project(Final).R
@@ -2,23 +2,12 @@
 # 기말고사 대체과제 2번 문제
 
 # 사용하는 라이브러리 올리기기
-# install.packages("rvest")
-# install.packages("XML")
+library(animation)
 
-library(rvest)
-library(XML)
 
 # 학번
 studentNumber = "20162908"
 
-# api관련 검색
-
-# api 정보
-api <- "http://openapi.q-net.or.kr/api/service/rest/InquiryRgnQualSVC/getYearList"
-api_key <-"lwDKSK7DSVTyedqIe8YCWlknOzQbgd6WN2LOD%2BMlnd1qIlCespH9fnunDeWfLWUmL3t2YLvJ5G5dXOloVYD%2BRA%3D%3D"
-
-
-# 특정년도 3년간 '기능장'자격층 취득정보 수집
 #요청변수 정보 및 설정
 
 # 시작년도 (2014년도)
@@ -34,57 +23,55 @@ seriesCd <- "02"  #계열코드
 quart <- "1"      #분기코드
 
 
-# 3년 데이터 탐색 시작
+# Q2에서 저장된 파일 불러오기
 
 for (baseYY in (baseYY:(baseYY+2))) { # baseYY포함하으로 + 2 
   
   # 4분기 반복
   for (quart in 1:4) {
     
-    # 진행 상황 출력
-    print(paste("전남지역",baseYY,"년도",quart,"분기 데이터"))
+    # 파일경로를 설정하는 path변수 지정
+    path = paste(studentNumber,"_",baseYY,"_",quart,".csv")
     
-    # 요청경로 설정
-    url <- paste(api,"?serviceKey=",api_key,"&rgnCd=",rgnCd,"&seriesCd=",seriesCd,"&baseYY=",baseYY,"&quart=",quart)
-    # url모든 공백 제거
-    url <- gsub("\\s", "", url)
-    
-    #xmlParse() : xml, HTML파일을 R에서 인식하는 구조로 변환
-    xmlFile <- xmlParse(url)
-    
-    
-    #xmlRott() : xml 문서 객체의 루트 노드에 접근
-    xmlRoot(xmlFile)
-    
-    
-    #xmlToDataFrma() : xml문서로부터 데이터 추출, 데이터프레임을 반환함
-    df <- xmlToDataFrame(getNodeSet(xmlFile, "//items/item")) # xml파일의 경로 지정 node를 뽑아 오기 위해
-    
-    #dim() : 객체의 차원을 조회
-    size <- dim(df)[1] # 데이터의 개수를 조회하여 size변수에 저장
-    
-    #저장하는 size결정 1개 이상 경우만 파일로 저장함
-    if (size > 0){
+    # 분기별 파일이 없는 경우를 탐색하며 예외처리를 통해 계속 진행
+    tryCatch(
       
-      print(paste("데이터의 개수", size,"저장"))
+      # 파일이 존재하는 경우
+      expr = {
+        df <- read.csv(path) # df변수에 데이터 저장
+        
+        # 진행 상황 출력
+        print(paste("전남지역",baseYY,"년도",quart,"분기 데이터"))
+        
+        jm <-c(df$jmNm) # 데이터에서 '종목'을 가지고있는 jmNm항목을 jm변수에 저장
+        count <- c(df$totcnt) # 데이터에서 '취득자 수'를 가지고 있는 totcnt항목을 count변수에 저장
+        
+        # 그래프 작성 과제 그래프 색상 Rainbow
+        result <- barplot(height = count, names.arg = jm, main = paste("전남지역",baseYY,"년도",quart,"분기"), 
+                          col = rainbow(length(count)), xlab="기능장 명", ylab="취득자 수",
+                          legend=jm) 
+        
+        # 그래프 출력
+        result
+        
+        #그래프 시간간격 설정
+        ani.pause(1)
+      },
+
+       
+       # 파일이 존재 하지 않는경우
+       error = function(e) {
+         next # 파일이 없는 경우 next진행 변수 e에는 에러정보 저장
+       },
       
-      #(매우중요) 현 디렉토리에 csv파일로 저장
-      setwd(getwd()) # 작업디렉토리를 현 경로로 설정
+      # 에러에 대한 메세지 출력
+       warning = function(e) print(paste(baseYY,"_",quart,"분기 파일 없음")), 
       
-      # 파일 이름 지정
-      path = paste(studentNumber,"_",baseYY,"_",quart,".csv")
-      
-      # 지정된 이름으로 파일 저장
-      write.csv(df,path)
-    }
-    
-    else{
-      print(paste("데이터의 개수가", size,"입니다 저장하지 않습니다."))
-    }
-    
+      # finally 기본값값
+       finally = NULL)
   }
 }
-# 2번문제 끝
+# 3번문제 끝
 
 
 
