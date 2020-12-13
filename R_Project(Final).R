6e5aa40c8b09c09ea17304556ebcf9febdaa79ee
명현 <meronic@naver.com>
2020-12-13 18:58:19 +0900

    good bye

--- R_Project(Final).R
+++ R_Project(Final).R
@@ -1,13 +1,15 @@
 # 컴퓨터소프트웨어공학과 20162908 유명현
 
 
-
-# 기말고사 대체과제 1번 문제
+####### 기말고사 대체과제 1번 문제 ########
 
 # install.packages(rvest) 패키지가 없다면 설치
 library(rvest) #사용할 라이브러리 등록
 
-# 1. 다음에서 본인 이름 검색
+# 대기시간 설정, 윈도우에서 버벅임
+Sys.sleep(3)
+
+# 다음에서 본인 이름 검색
 url.api <- "https://search.daum.net/search?w=web&DA=PGD&enc=utf8&lpp=10&q=%EC%9C%A0%EB%AA%85%ED%98%84&p="
 
 
@@ -15,7 +17,7 @@ titles <- NULL # 문서 타이틀들을 담을 변수
 addressALL <- NULL # 주소들을 담을 변수
 
 
-# 2. 웹문서 내용 3페이지까지 스크랩핑
+# 웹문서 내용 3페이지까지 스크랩핑
 for (page in 1:3){
   
   url <- paste(url.api, page, sep="")
@@ -46,17 +48,20 @@ for (page in 1:3){
 }
 
 
-#3. '문서 제목', '링크사이트주소' 2가지 내용 가져와서 데이터 프레임으로 저장
+#'문서 제목', '링크사이트주소' 2가지 내용 가져와서 데이터 프레임으로 저장
 api = data.frame(title=titles, www=addressALL)
 
-#4. 테이블 형식으로 출력
+#테이블 형식으로 출력
 View(api)
 
-# 1번문제 끝
+#### 출력시 대기시간 설정 윈도우에서 실행시 버벅임 #####
+Sys.sleep(3)
+
+######## 1번문제 끝 ##############
 
 
 
-# 기말고사 대체과제 2번 문제
+############ 기말고사 대체과제 2번 문제 ###########
 
 # 사용하는 라이브러리 올리기기
 # install.packages("rvest") 패키지가 없다면 설치
@@ -138,11 +143,14 @@ for (baseYY in (baseYY:(baseYY+2))) { # baseYY포함하으로 + 2
     else{
       print(paste("데이터의 개수가", size,"입니다 저장하지 않습니다."))
     }
-    
   }
 }
+########## 2번문제 끝 ###########
 
-# 기말고사 대체과제 3번 문제
+
+
+
+########## 기말고사 대체과제 3번 문제 ##########
 
 # 사용하는 라이브러리 올리기기
 library(animation)
@@ -175,61 +183,43 @@ for (baseYY in (baseYY:(baseYY+2))) { # baseYY포함하으로 + 2
     # 파일경로를 설정하는 path변수 지정
     path = paste(studentNumber,"_",baseYY,"_",quart,".csv")
     
-    # 분기별 파일이 없는 경우를 탐색하며 예외처리를 통해 계속 진행
-    tryCatch(
+    # file.exists함수를 사용하여 파일의 유무를 확인하고 진행
+    fileCheck <- file.exists(path) 
+    
+    # csv파일이 존재하는 경우
+    if (fileCheck == TRUE){
       
-      # 파일이 존재하는 경우
-      expr = {
-        df <- read.csv(path) # df변수에 데이터 저장
-        
-        # 진행 상황 출력
-        print(paste("전남지역",baseYY,"년도",quart,"분기 데이터"))
-        
-        jm <-c(df$jmNm) # 데이터에서 '종목'을 가지고있는 jmNm항목을 jm변수에 저장
-        count <- c(df$totcnt) # 데이터에서 '취득자 수'를 가지고 있는 totcnt항목을 count변수에 저장
-        
-        # 그래프 작성 과제 그래프 색상 Rainbow #legend추가
-        # 실행시 legend 사이즈가 크게 출력됩니다. 창의 크기를 충분히 늘려주세요
-        result <- barplot(height = count, names.arg = jm, main = paste("전남지역",baseYY,"년도",quart,"분기"), 
-                          col = rainbow(length(count)), xlab="기능장 명", ylab="취득자 수",
-                          legend=jm) 
-        # 그래프 출력
-        result
-        
-        # 그래프의 평균 선 추가
-        abline(h=mean(count), col="orange", lty=1)
-        
-        
-        #그래프 시간간격 설정
-        ani.pause(1)
-      },
+      df <- read.csv(path) # df변수에 데이터 저장
+      # 진행 상황 출력
+      print(paste("전남지역",baseYY,"년도",quart,"분기 데이터"))
+      
+      jm <-c(df$jmNm) # 데이터에서 '종목'을 가지고있는 jmNm항목을 jm변수에 저장
+      count <- c(df$totcnt) # 데이터에서 '취득자 수'를 가지고 있는 totcnt항목을 count변수에 저장
+      
+      # 그래프 작성 과제 그래프 색상 Rainbow #legend추가
+      # 실행시 legend 사이즈가 크게 출력됩니다. 창의 크기를 충분히 늘려주세요
+      result <- barplot(height = count, names.arg = jm, main = paste("전남지역",baseYY,"년도",quart,"분기"), 
+                        col = rainbow(length(count)), xlab="기능장 명", ylab="취득자 수",
+                        legend=jm) 
+      # 그래프 출력
+      result
+      
+      # 그래프의 평균 선 추가
+      abline(h=mean(count), col="orange", lty=1)
       
       
-      # 파일이 존재 하지 않는경우
-      error = function(e) {
-        next # 파일이 없는 경우 next진행 변수 e에는 에러정보 저장
-      },
-      
-      # 에러에 대한 메세지 출력
-      warning = function(e) print(paste(baseYY,"_",quart,"분기 파일 없음")), 
-      
-      # finally 기본값값
-      finally = NULL)
+      #그래프 시간간격 설정
+      ani.pause(1)
+    }
+    
+    
+    # 파일이 존재하지 않는 경우 메세지 출력
+    else{
+      print(paste(baseYY,"_",quart,"분기 파일 없음"))
+    }
   }
 }
-# 3번문제 끝
-
-
-# 기말고사 1번문제 실행 영상 (비공개 링크)
-# https://youtu.be/hlu9KTECLNw
-
-# 기말고사 2번문제 실행 영상 (비공개 링크)
-# https://youtu.be/dHkKVcjwTmg
-
-# 기말고사 3번문제 실행 영상 (비공개 링크)
-# https://youtu.be/9Lp1HV7GVkM
-
-
+######## 3번문제 끝 ###########
 
 
 
